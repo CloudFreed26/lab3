ffc8fbfbb0c495d5cdc42c6c5b8dadc03488d007
lab303-6 <lab303-6@labeng.local>
2019-01-11 17:42:58 +0700

    lab3_5.cpp

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,18 +1,34 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
 int main()
 {
-    float answer=0,x=0,y=6 ;
+    string text1,text2;
+    int N,count=0;
 
-    while(y<=19)
+    cout<<"Enter the first text: ";
+    cin>>text1;
+    cout<<"Enter the second text: ";
+    cin>>text2;
+    cout<<"Enter N: ";
+    cin>>N;
+    while(count<N)
     {
-        x = 1/y;
-        y++;
-        answer += x; 
-        cout<<answer<<endl;
+        if (count%2==0)
+        {
+            cout<<text1<<" ";
+        }
+        else
+        {
+            cout<<text2<<" ";
+        }
+        count++;
     }
+
+
+    
     return 0;
 }
 
